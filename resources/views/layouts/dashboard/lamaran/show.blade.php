49a0469e02c86fbb108f42ccca5336087fc266f8
nuhptr <nugrahaadi733@gmail.com>
2021-11-27 11:15:19 +0700

    update

--- resources/views/layouts/dashboard/lamaran/show.blade.php
+++ resources/views/layouts/dashboard/lamaran/show.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    {{-- <x-slot name="script">
+    <x-slot name="script">
         <script>
             // AJAX DataTable
             var datatable = $('#crudTable').DataTable({
@@ -15,12 +15,11 @@
                 columns: [
                     { data: 'id', name: 'id', width: '5%'},
                     { data: 'user.name', name: 'user.name' },
-                    { data: 'pekerjaan.nama_pekerjaan', name: 'pekerjaan.nama_pekerjaan' },
-                    { data: 'pekerjaan.nama_perusahaan', name: 'pekerjaan.nama_perusahaan' },
+                    { data: 'user.alamat', name: 'user.alamat' },
                 ],
             });
         </script>
-    </x-slot> --}}
+    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -29,8 +28,12 @@
                  <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table-auto w-full">
                         <tbody>
-                            <tr>
-                                <img class="py-4" src="{{ Storage::url($lamaran->user->profile_photo_path ?? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png') }}" width="200" height="200"/>
+                             <tr>
+                                <th class="border px-6 py-4 text-left">Foto User</th>
+                                <td class="border px-6 py-4">
+                                    <img class="py-4" src="{{ Storage::url($lamaran->user->profile_photo_path ?? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png') }}" width="200" height="200"/>
+                                </td>
+                                
                             </tr>
                             <tr>
                                 <th class="border px-6 py-4 text-left">Name</th>
@@ -49,38 +52,6 @@
                                 <td class="border px-6 py-4">{{ $lamaran->pekerjaan->lokasi_pekerjaan }}</td>
                             </tr>
                             <tr>
-                                <th class="border px-6 py-4 text-left">Tempat Terakhir Bekerja</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->tempat_terakhir_bekerja }}</td>
-                            </tr>
-                            <tr>
-                                <th class="border px-6 py-4 text-left">Posisi Terakhir Bekerja</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->posisi_terakhir_bekerja }}</td>
-                            </tr>
-                            <tr>
-                                <th class="border px-6 py-4 text-left">Prestasi</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->prestasi }}</td>
-                            </tr>
-                             <tr>
-                                <th class="border px-6 py-4 text-left">Skill</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->skill }}</td>
-                            </tr>
-                             <tr>
-                                <th class="border px-6 py-4 text-left">Pendidikan</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->pendidikan }}</td>
-                            </tr>
-                             <tr>
-                                <th class="border px-6 py-4 text-left">Kewarganegaraan</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->kewarganegaraan }}</td>
-                            </tr>
-                             <tr>
-                                <th class="border px-6 py-4 text-left">Alamat</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->alamat }}</td>
-                            </tr>
-                             <tr>
-                                <th class="border px-6 py-4 text-left">Prestasi</th>
-                                <td class="border px-6 py-4">{{ $lamaran->user->prestasi }}</td>
-                            </tr>
-                            <tr>
                                 <th class="border px-6 py-4 text-left">Gaji</th>
                                 <td class="border px-6 py-4">{{ $lamaran->pekerjaan->gaji }}</td>
                             </tr>
@@ -92,12 +63,18 @@
                                 <th class="border px-6 py-4 text-left">Kategori</th>
                                 <td class="border px-6 py-4">{{ $lamaran->pekerjaan->kategori }}</td>
                             </tr>
+                            <tr>
+                                <th class="border px-6 py-4 text-left">Lampiran CV</th>
+                                <td class="border px-6 py-4">
+                                    <img class="py-4" src="{{ $lamaran->user->cv_path ?? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png' }}" width="200" height="200"/>
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
 
-            {{-- <h2 class="font-semibold text-lg text-gray-800 leading-tight mb-5">List Lamaran</h2>
+            <h2 class="font-semibold text-lg text-gray-800 leading-tight mb-5">List Lamaran</h2>
             <div class="shadow overflow-hidden sm:rounded-md">
                 <div class="px-4 py-5 bg-white sm:p-6">
                     <table id="crudTable">
@@ -105,14 +82,13 @@
                         <tr>
                             <th>ID</th>
                             <th>Nama User</th>
-                            <th>Nama Pekerjaan</th>
-                            <th>Nama Perusahaan</th>
+                            <th>Alamat</th>
                         </tr>
                         </thead>
                         <tbody></tbody>
                     </table>
                 </div>
-            </div> --}}
+            </div>
         </div>
     </div>
 </x-app-layout>
